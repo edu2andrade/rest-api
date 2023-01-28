51d3b826c95e8dc8157b8d88bc6865e9565c5db4
Eduardo <edu2andrade@gmail.com>
2023-01-28 20:49:49 +0100

    Validation implemented to createPost route

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const feedController = require('../controllers/feed')
 
+const { body } = require('express-validator/check')
+
 const router = express.Router()
 
 // 'GET' (Method) '/feed/post' (Route)
 router.get('/posts', feedController.getPosts)
 // POST request to /feed/posts
-router.post('/posts', feedController.createPost)
+router.post('/posts', [
+  body('title').trim().isLength({min: 5}),
+  body('content').trim().isLength({min: 5})
+], feedController.createPost)
 
 module.exports = router
